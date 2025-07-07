9194ec42c480be3559cbbbc4fce5d40ad49e185e
Ozzie Martin <ozzie.14.martin@gmail.com>
2025-07-07 11:53:16 -0400

    Add dist

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { fileURLToPath, URL } from 'node:url';
-
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
-
 // https://vitejs.dev/config/
 export default defineConfig({
     base: '/vue-graph-algo-demo/',
