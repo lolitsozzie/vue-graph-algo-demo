70835b9d034ef3d421f6acc1fe718d3aa1e11741
Ozzie Martin <ozzie.14.martin@gmail.com>
2025-07-07 20:28:02 -0400

    Adjust height on Kahns algorithm

--- src/views/algorithms/Kahns.vue
+++ src/views/algorithms/Kahns.vue
@@ -7,7 +7,7 @@ import graph from '@/components/graph/BreadthFirst.vue';
     <h2 class="text-xl font-semibold h-full py-auto">
         Kahn’s Algorithm
     </h2>
-    <div class="w-full h-[32rem] flex items-center justify-center bg-gray-100 rounded-md">
+    <div class="w-full h-[40rem] flex items-center justify-center bg-gray-100 rounded-md">
         <graph />
     </div>
 </template>
