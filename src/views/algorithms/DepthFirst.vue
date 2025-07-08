f473f3ab179ea8259c21fe2385db29abacdf3a9e
Ozzie Martin <ozzie.14.martin@gmail.com>
2025-07-07 20:27:48 -0400

    Add depth first simulator

--- src/views/algorithms/DepthFirst.vue
+++ src/views/algorithms/DepthFirst.vue
@@ -1,13 +1,13 @@
 <script setup>
 import { computed } from 'vue';
-import graph from '@/components/graph/BreadthFirst.vue';
+import DepthFirst from '@/components/graph/DepthFirst.vue';
 </script>
 
 <template>
     <h2 class="text-xl font-semibold h-full py-auto">
         Depth First Search
     </h2>
-    <div class="w-full h-[32rem] flex items-center justify-center bg-gray-100 rounded-md">
-        <graph />
+    <div class="w-full h-[40rem] flex items-center justify-center bg-gray-100 rounded-md">
+        <DepthFirst />
     </div>
 </template>
